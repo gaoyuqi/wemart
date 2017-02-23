7613c89d3c51fef3c4542fdb84499a8c38687fe8
seanMYG <13818815870@163.com>
2017-02-23 18:22:48 +0800

    update the test case

--- src/cn/wemart/TestCase/PostTemplateTest.java
+++ src/cn/wemart/TestCase/PostTemplateTest.java
@@ -5,11 +5,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.testng.Reporter;
+
 import com.TestNG.Assertion;
+
 import cn.wemart.objectbase.ObjectBase;
 import cn.wemart.userManagment.ShopLogin;
 import cn.wemart.util.GetDataDB;
@@ -21,6 +24,7 @@ public class PostTemplateTest {
 	String mobile = "13818881111";
 	String password = "123456";
 	String sellerId = "234";
+	String faretmpltNo = null;
 	
 	CloseableHttpClient httpClient = HttpClients.createDefault();
 	ObjectBase PTT = new ObjectBase();
@@ -55,6 +59,7 @@ public class PostTemplateTest {
 		String returnValue = PTT.Test(httpClient, keyValueList);
 		System.out.println(PTT.response);
 		if (Assertion.verifyEqual(returnValue, "0")) {
+			faretmpltNo = JSONObject.fromObject(PTT.response).getString("data");
 			Reporter.log("创建邮费模板成功！");
 			System.out.println("创建邮费模板成功");
 		} else {
@@ -65,24 +70,107 @@ public class PostTemplateTest {
 	}
 	
 	public void UpdatePostTemplate(){
-		
+		PTT.Init("put", url);
+		String dataBaseName = "wmplatform_gm";
+		String tableName = "tbl_gm_citydef";
+		String outPutColumnName = "city_no";
+		String condition = "limit 5";
+		GetDataDB getData = new GetDataDB();
+		ArrayList<String> cityNoList = getData.sql(dataBaseName, tableName,outPutColumnName, condition);
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("cityNoList", cityNoList);
+		map.put("increFare", 5);
+		map.put("increNum", 1);
+		map.put("initFare", 10);
+		map.put("initNum", 2);
+		JSONArray fareList = JSONArray.fromObject(map);
+		if(faretmpltNo != null){
+			Map<String,Object> keyValueList = new HashMap<String,Object>();
+			keyValueList.put("fareList", fareList);
+			keyValueList.put("faretmpltNo", faretmpltNo);
+			keyValueList.put("faretmpltName", "UpdateFaretmpltNameTest");
+	
+			String returnValue = PTT.Test(httpClient, keyValueList);
+			System.out.println(PTT.response);
+			if (Assertion.verifyEqual(returnValue, "0")) {
+				
+				Reporter.log("修改邮费模板成功！");
+				System.out.println("修改邮费模板成功");
+			} else {
+				Reporter.log("修改邮费模板失败！\n Response：" + PTT.response);
+				System.out.println("修改邮费模板失败！\n Response：" + PTT.response);
+			}
+		}
+		Reporter.log(getCurrent.Time());
 	}
 	
 	public void GetPostTemplate(){
-		
+		PTT.Init("get", url);
+		Map<String,Object> keyValueList = new HashMap<String,Object>();
+		String returnValue = PTT.Test(httpClient, keyValueList);
+		System.out.println(PTT.response);
+		if (Assertion.verifyEqual(returnValue, "0")) {
+			Reporter.log("查询邮费模板成功！");
+			System.out.println("查询邮费模板成功");
+		} else {
+			Reporter.log("查询邮费模板失败！\n Response：" + PTT.response);
+			System.out.println("查询邮费模板失败！\n Response：" + PTT.response);
+		}
+		Reporter.log(getCurrent.Time());
 	}
 	
 	public void DeletePostTemplate(){
-		
+		PTT.Init("delete", url);
+		if(faretmpltNo != null){ 
+			Map<String,Object> keyValueList = new HashMap<String,Object>();
+			keyValueList.put("faretmpltNo", faretmpltNo);
+			keyValueList.put("altFaretmpltNo", "");
+			String returnValue = PTT.Test(httpClient, keyValueList);
+			System.out.println(PTT.response);
+			if (Assertion.verifyEqual(returnValue, "0")) {
+				Reporter.log("删除邮费模板成功！");
+				System.out.println("删除邮费模板成功");
+			} else {
+				Reporter.log("删除邮费模板失败！\n Response：" + PTT.response);
+				System.out.println("删除邮费模板失败！\n Response：" + PTT.response);
+			}
+			Reporter.log(getCurrent.Time());
+		}
 	}
 
 	public void GetGoodsPostTemplate(){
+		String url = LoadAPIInfo.url + "/api/goodsmng/faretmplt/goods";
+		PTT.Init("get", url);
+		String dataBaseName = "wmplatform_gm";
+		String tableName = "tbl_gm_goods";
+		String outPutColumnName = "goods_id";
+		String condition = "limit 5";
+		GetDataDB getData = new GetDataDB();
+		ArrayList<String> goodsIdList = getData.sql(dataBaseName, tableName,outPutColumnName, condition);
+		for(String goodsId :goodsIdList){
+			Map<String,Object> keyValueList = new HashMap<String,Object>();
+			keyValueList.put("goodsId", goodsId);
+			String returnValue = PTT.Test(httpClient, keyValueList);
+			System.out.println(PTT.response);
+			if (Assertion.verifyEqual(returnValue, "0")) {
+				Reporter.log("查询商品邮费模板成功！");
+				System.out.println("查询商品邮费模板成功");
+			} else {
+				Reporter.log("查询商品邮费模板失败！\n Response：" + PTT.response);
+				System.out.println("查询商品邮费模板失败！\n Response：" + PTT.response);
+			}
+			Reporter.log(getCurrent.Time());
+		}
 		
 	}
 	
 	public static void main(String[] args) {
 		PostTemplateTest ptt = new PostTemplateTest();
 		ptt.CreatePostTemplate();
+		ptt.UpdatePostTemplate();
+		ptt.GetPostTemplate();
+		ptt.DeletePostTemplate();
+		ptt.GetGoodsPostTemplate();
 	}
 
 }
